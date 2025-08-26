a8b78a20b8e493a715976146335dc788488ef929
siddharths060 <siddharthsrivishwa@gmail.com>
2025-08-26 23:47:32 +0530

    Properties of js

--- week-1/basics-of-js/node1.js
+++ week-1/basics-of-js/node1.js
@@ -1,5 +1,17 @@
 /* browser understand HTML,CSS,Js and Web assembly
    technically the brower can run C/C++ code using web assembly
    Properties of JS:
+   1.Interpreted
+   Js is an interperted language, ie: it is executed line by line at runtime
+   by the javascript engine in the brower or server environment 
+
+   2.Dynamically typed language
+   ie, the variables of js are not bound to a specific data type
    
+   3.
 */
+
+var a = 10;
+console.log(a);
+a = "sid"
+console.log(a);
